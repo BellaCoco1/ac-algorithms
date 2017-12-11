2e829d64c5fbd87a24b109f61bee5db6e53b695c
bellacoco <bellacoco@anniecannons.com>
2017-12-11 14:33:49 -0800

    completed circleCalculations exercise

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -1,10 +1,19 @@
 //Write a JavaScript program to calculate the area and perimeter of a circle. The radius of the circle will be supplied by the user
 
+
 //Bonus 1: Write the answer like this "The area of the circle is ___ and the perimeter of the circle is ____"
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
+// input: radious of the circle
+// output: te solved calculation, which is the area and perimeter of the circle (with that radious)
 
-function circleCalculation () {
-	//your code here
+//figure out pi
+	// area of a circle is PI * (radios * radius)
 
+function circleCalculation (radius) {
+	
+	var area = Math.PI (radius * radius);
+	var perimeter = 2 * Math.PI * radius;
+	document.write("The area of the circle is " + Math.round(area) + " and the perimeter of the circle is " + Math.round(perimeter))
+}
 circleCalculation(10);
